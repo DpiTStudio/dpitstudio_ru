820b70f853514b974b3927d555c4a069e276cb84
DpiTStudio <admin@dpitstudio.ru>
2025-06-20 18:21:56 +0300

    On branch addmain Your branch is up to date with 'origin/addmain'.
    Changes to be committed:
    	modified:   app/app/__pycache__/settings.cpython-311.pyc
    	modified:   app/app/__pycache__/urls.cpython-311.pyc
    	modified:   app/app/urls.py
    	new file:   app/main/__pycache__/views.cpython-311.pyc
    	modified:   app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request) -> HttpResponse:
+    # return render(request, "index.html")
+    return HttpResponse("Hello world index")
+
+
+def about(request) -> HttpResponse:
+    # return render(request, "about.html")
+    return HttpResponse("Hello world about")
