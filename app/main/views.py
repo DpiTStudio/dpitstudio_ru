c5654827bb4bbdbcd49ae0be87c4182352bf7458
DpiTStudio <admin@dpitstudio.ru>
2025-06-20 23:23:43 +0300

    On branch main_1 Your branch is up to date with 'origin/main_1'.
    Changes to be committed:
    	modified:   app/main/__pycache__/views.cpython-311.pyc
    	new file:   app/main/templates/main/about.html
    	modified:   app/main/templates/main/index.html
    	modified:   app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -8,10 +8,17 @@ def index(request):
         "content": "Добро пожаловать на главную страницу нашего сайта. Здесь вы можете найти информацию о нашем проекте и наших услугах. Если у вас есть какие-либо вопросы или пожелания, пожалуйста, свяжитесь с нами. Мы будем рады помочь вам.",
         "list": ["first", "second"],
         "dict": {"first": 1},
-        "is_aut": False,
+        "is_aut": True,
     }
     return render(request, "main/index.html", context=context)
 
 
-def about(request) -> HttpResponse:
-    return render(request, "about.html")
+def about(request):
+    context = {
+        "title": "Страница о нас DPITSTUDIO",
+        "content": "Истоия нас и наши услуги",
+        "list": ["first", "second"],
+        "dict": {"first": 1},
+        "is_aut": True,
+    }
+    return render(request, "main/about.html", context=context)
