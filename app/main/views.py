75d7c75e4627069fc85cf7a72e302a8eb3a6dcc0
DpiTStudio <admin@dpitstudio.ru>
2025-06-20 21:09:15 +0300

    On branch addmain Your branch is up to date with 'origin/addmain'.
    Changes to be committed:
    	modified:   app/main/__pycache__/views.cpython-311.pyc
    	modified:   app/main/templates/main/index.html
    	modified:   app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -8,7 +8,7 @@ def index(request):
         "content": "Добро пожаловать на главную страницу нашего сайта. Здесь вы можете найти информацию о нашем проекте и наших услугах. Если у вас есть какие-либо вопросы или пожелания, пожалуйста, свяжитесь с нами. Мы будем рады помочь вам.",
         "list": ["first", "second"],
         "dict": {"first": 1},
-        "is_aut": True,
+        "is_aut": False,
     }
     return render(request, "main/index.html", context=context)
 
