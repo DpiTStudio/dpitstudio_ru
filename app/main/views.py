e5a9604f7052a6816df3f78dfcc3c1eb19034cd2
Tigran <62198325+DpiTStudio@users.noreply.github.com>
2025-06-21 17:09:06 +0300

    Revert "Detached"

--- app/main/views.py
+++ app/main/views.py
@@ -3,18 +3,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    con_text = {
+    context = {
         "title": "Главная страница сайта DPITSTUDIO",
         "content": "Добро пожаловать на главную страницу нашего сайта. Здесь вы можете найти информацию о нашем проекте и наших услугах. Если у вас есть какие-либо вопросы или пожелания, пожалуйста, свяжитесь с нами. Мы будем рады помочь вам.",
-        "is_aut": True,
+        "list": ["first", "second"],
+        "dict": {"first": 1},
+        "is_aut": False,
     }
-    return render(request, "main/index.html", context=con_text)
+    return render(request, "main/index.html", context=context)
 
 
-def about(request):
-    con_text = {
-        "title": "Страница о нас DPITSTUDIO",
-        "content": "Истоия нас и наши услуги",
-        "is_aut": True,
-    }
-    return render(request, "main/about.html", context=con_text)
+def about(request) -> HttpResponse:
+    return render(request, "about.html")
