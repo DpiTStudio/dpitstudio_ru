c51efd2f25feb427be5d2fc49e9a380eb0888351
DpiTStudio <admin@dpitstudio.ru>
2025-06-25 16:39:52 +0300

    On branch main_1 Your branch is up to date with 'origin/main_1'.
    Changes to be committed:
    	modified:   app/main/__pycache__/views.cpython-311.pyc
    	modified:   app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request):
+def index(request) -> HttpResponse:
     con_text = {
         "title": "Главная страница сайта DPITSTUDIO",
         "content": "Добро пожаловать на главную страницу нашего сайта. Здесь вы можете найти информацию о нашем проекте и наших услугах. Если у вас есть какие-либо вопросы или пожелания, пожалуйста, свяжитесь с нами. Мы будем рады помочь вам.",
@@ -11,10 +11,10 @@ def index(request):
     return render(request, "main/index.html", context=con_text)
 
 
-def about(request):
+def about(request) -> HttpResponse:
     con_text = {
         "title": "Страница о нас DPITSTUDIO",
-        "content": "Истоия нас и наши услуги",
+        "content": "История нас и наши услуги",
         "is_aut": True,
     }
     return render(request, "main/about.html", context=con_text)
